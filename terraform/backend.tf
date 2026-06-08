42d8eb6b62bf4dc526ea45a98f2f7dab75b07031
Raymond Bautista <raymond.bautista0208@gmail.com>
2026-06-08 01:48:06 -0400

    guardados

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "gestor-usuarios"
+    bucket = "gestor-usuarios/gestor-usuarios"
     key    = "gestor-usuarios/terraform.tfstate"
     region = "us-east-2"
   }
