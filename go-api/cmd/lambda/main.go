7a0cee570b059b0f81e40d724dd2e2e3f92dd851
Raymond Bautista <raymond.bautista0208@gmail.com>
2026-07-05 14:41:38 -0400

    guardado52

--- go-api/cmd/lambda/main.go
+++ go-api/cmd/lambda/main.go
@@ -67,7 +67,11 @@ func main() {
 		ctx context.Context,
 		req events.APIGatewayV2HTTPRequest,
 	) (events.APIGatewayV2HTTPResponse, error) {
-		log.Println("Lambda ejecutada por EventBridge")
+
+		if req.RequestContext.HTTP.Method == "" {
+			log.Println("Lambda ejecutada por EventBridge")
+		}
+
 		return ginLambda.ProxyWithContext(ctx, req)
 	})
 }
